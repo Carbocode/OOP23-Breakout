51403ed2a4560d609b38396e2bb8f40a00bfe063
Yosberto Baro Carbonell <yosberto.baro@studio.unibo.it>
2024-01-24 16:02:58 +0100

    Classi Base per posizione

---
+++ src/main/java/it/unibo/api/Direction.java
@@ -0,0 +1,14 @@
+public class Direction {
+    private int x;
+    private int y;
+    public Direction(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getHorizontalVelocity(){
+        return x;
+    }
+    public int GetVerticalVelocity(){
+        return y;
+    }
+}
