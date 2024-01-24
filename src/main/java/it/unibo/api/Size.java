8521c743c909a1c1012254abfa11894e23c17c95
Yosberto Baro Carbonell <yosberto.baro@studio.unibo.it>
2024-01-24 16:02:58 +0100

    Classi Base per posizione

---
+++ src/main/java/it/unibo/api/Size.java
@@ -0,0 +1,14 @@
+public class Size {
+    private int w;
+    private int h;
+    public Size(int w,int h){
+        this.w = w;
+        this.h = h;
+    }
+    public int getWidth(){
+        return w;
+    }
+    public int getHeight(){
+        return h;
+    }
+}
