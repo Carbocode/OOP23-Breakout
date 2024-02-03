7b690afca8438e43ab48d3c6c9ed2baa8fbd497a
Giacomo Ruscelli <giacomo.ruscelli2@studio.unibo.it>
2024-02-03 10:34:27 +0100

    code completed, needs to be tested

---
+++ src/test/java/it/unibo/DashboardTest.java
@@ -0,0 +1,14 @@
+package test.java.it.unibo;
+
+import static org.junit.Assert.*;
+import java.util.*;
+import src.main.java.unibo.*;
+
+public class DashboardTest {
+
+    @org.junit.Test
+	public void testTop10() {
+		
+	}
+    
+}
