73dd41644efbc4e438408760c71ce4433cc67464
Giacomo Ruscelli <giacomo.ruscelli2@studio.unibo.it>
2024-03-02 11:52:06 +0100

    added gameEntity Support

---
+++ src/main/java/it/unibo/model/BarImpl.java
@@ -0,0 +1,18 @@
+package main.java.it.unibo.model;
+import java.awt.*;
+
+public class BarImpl implements GameEntity,Bar{
+    
+
+    
+    final int length = 10; //lentgh of the rectangle
+
+    void move(boolean direction,int distance){
+        
+        if(direction){
+            //direction left
+        }
+    }
+
+    
+}
