0283e67bae2be4c3947da2864ff5eadd75311e76
Sohail Mama <sohail.mama@studio.unibo.it>
2024-03-23 12:54:59 +0100

    created the menu feature, need to be adjusted the font and the image

---
+++ src/main/java/it/unibo/view/Menu.java
@@ -0,0 +1,88 @@
+package it.unibo.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Menu extends JFrame {
+    JPanel mainPanel;
+    JPanel gamePanel;
+    JLabel titleLabel;
+    JButton playButton;
+    JButton scoreboardButton;
+    JButton exitButton;
+
+    public Menu() {
+        setTitle("Breakout");
+        setSize(800, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // creation of the panel of the menu
+        mainPanel = new JPanel(new GridLayout(3, 1));
+        gamePanel = new JPanel();
+        titleLabel = new JLabel("BREAKOUT", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+
+        // creating the background image
+        ImageIcon backgroundImage = new ImageIcon("../api/appdata/space.jpg");
+        JLabel backgroundLabel = new JLabel(backgroundImage);
+        backgroundLabel.setLayout(new BorderLayout());
+
+        // these are the buttons of the menu with their panel
+        JPanel buttonPanel = new JPanel(new GridLayout(3, 1, 0, 10));
+        buttonPanel.setOpaque(false);
+
+        playButton = new JButton("PLAY");
+        playButton.setPreferredSize(new Dimension(100, 50));
+
+        scoreboardButton = new JButton("SCOREBOARD");
+        scoreboardButton.setPreferredSize(new Dimension(100, 50));
+
+        exitButton = new JButton("EXIT");
+        exitButton.setPreferredSize(new Dimension(100, 50));
+
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TO DO
+            }
+        });
+
+        scoreboardButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TO DO
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        buttonPanel.add(playButton);
+        buttonPanel.add(scoreboardButton);
+        buttonPanel.add(exitButton);
+
+        backgroundLabel.add(titleLabel, BorderLayout.NORTH);
+        backgroundLabel.add(buttonPanel, BorderLayout.CENTER);
+
+        mainPanel.add(backgroundLabel, BorderLayout.CENTER);
+        add(mainPanel);
+        setFocusable(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Menu menu = new Menu();
+                menu.setVisible(true);
+            }
+        });
+    }
+}
