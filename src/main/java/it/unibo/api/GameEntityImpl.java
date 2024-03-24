06a0ca323fef7e41c6690d434f5f61c5a513177f
Vincenzo Prisco <vincenzo.prisco@studio.unibo.it>
2024-03-24 16:42:53 +0100

    fix GameEntity Getter Setter and Constructor

--- src/main/java/it/unibo/api/GameEntityImpl.java
+++ src/main/java/it/unibo/api/GameEntityImpl.java
@@ -1,21 +1,38 @@
 package it.unibo.api;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 
 public abstract class GameEntityImpl implements GameEntity {
 
     final static int IMMORTAL_ENTITY_HEALTH = -1;
-    private int health;
-    protected Point pos;
-    protected Dimension size;
 
-    public GameEntityImpl(Point pos,Dimension size, int health) {
-        this.pos = pos;
+    protected Point position;
+    protected Dimension size;
+    protected Color color;
+    protected int health;
+
+    public GameEntityImpl(Point position, Dimension size, int health, Color color) {
+        this.position = position;
         this.size = size;
         this.health = health;
+        this.color = color;
     }
 
+    public GameEntityImpl(Point position, Dimension size, Color color) {
+        this.position = position;
+        this.size = size;
+        this.health = IMMORTAL_ENTITY_HEALTH;
+        this.color = color;
+    }
+
+    public GameEntityImpl(Point position, Dimension size) {
+        this.position = position;
+        this.size = size;
+        this.health = IMMORTAL_ENTITY_HEALTH;
+        this.color = new Color(70, 70, 70);
+    }
 
     @Override
     abstract public void onCollision();
@@ -26,13 +43,43 @@ public abstract class GameEntityImpl implements GameEntity {
     }
 
     @Override
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+    @Override
+    public void setSize(Dimension size) {
+        this.size = size;
+    }
+
+    @Override
     public void setHealth(int health) {
         this.health = health;
     }
 
     @Override
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public Point getPosition() {
+        return position;
+    }
+
+    @Override
+    public Dimension getSize() {
+        return size;
+    }
+
+    @Override
     public int getHealth() {
         return health;
+    }
+
+    @Override
+    public Color getColor() {
+        return color;
     }
 
 }
