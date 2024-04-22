6d46134cd2be9dbe6a3137c93d82b7a4986a75d6
Sohail Mama <sohail.mama@studio.unibo.it>
2024-04-22 11:23:03 +0200

    changes

--- src/main/java/it/unibo/view/Game.java
+++ src/main/java/it/unibo/view/Game.java
@@ -18,7 +18,7 @@ public class Game extends JFrame {
     private ArrayList<Brick> bricks;
     private Ball ball;
     long time;
-    private Paddle paddle;
+    private Bar bar;
     private int points = 0;
     private int lives = 3;
     private int level = 1;
@@ -26,12 +26,44 @@ public class Game extends JFrame {
     private boolean gamePaused = false;
     private boolean playerIsDead = false;
 
+    private Graphics2D g2;
+
     
     public Game() {
         setTitle("Breakout");
         setSize(measure.getGameAreaWidth(), measure.getGameAreaHeight());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setFocusable(true);
+
+    }
+
+    private void gameInit(){
+        ball=new Ball();
+        bar=new BarImpl(getMousePosition(), getSize(), ABORT, getForeground());
+        //bricks
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponents(g);
+        g2=(Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
+        if(inGame){
+            drawObjects(g2);
+        }else{
+            gameFinished(g2);
+        }
+
+    }
+
+    private void drawObjects(Graphics2D g2) {
+        g2.drawImage("../appdata/images/ball.png", ball.getSize());
+    }
+
+    private void gameFinished(Graphics2D g2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'gameFinished'");
     }
 
 
