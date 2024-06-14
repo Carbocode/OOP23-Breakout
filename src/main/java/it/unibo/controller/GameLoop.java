b922b235ff0ef1078c61ced2b7a636fb69619364
Giacomo Ruscelli <giacomo.ruscelli03@gmail.com>
2024-06-14 14:47:51 +0200

    fixed bugs

--- src/main/java/it/unibo/controller/GameLoop.java
+++ src/main/java/it/unibo/controller/GameLoop.java
@@ -1,27 +1,25 @@
 package it.unibo.controller;
 
+
 import it.unibo.api.BrickWall;
 import it.unibo.api.CollisionManager;
 import it.unibo.api.GameInfo;
 import it.unibo.api.SoundManager;
 import it.unibo.model.Ball;
-import it.unibo.model.BarImpl;
-import it.unibo.view.GameView;
+import it.unibo.model.Bar;
 import it.unibo.view.SoundManagerImpl;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import java.util.Set;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.util.HashSet;
-import javax.swing.Timer;
+import it.unibo.view.*;
 
-
-/**
- * The GameLoop class implements the main game loop for updating game state
- * and rendering the game. It uses a Swing Timer to trigger periodic updates.
- */
 public class GameLoop implements ActionListener {
     private static final long UPDATE_INTERVAL = 1000 / GameInfo.REFRESH_RATE;
 
@@ -29,84 +27,63 @@ public class GameLoop implements ActionListener {
     private SoundManager soundPlayer;
     private BrickWall brickWall;
     private Set<Ball> balls;
-    private BarImpl paddle;
+    private Bar paddle;
 
     private long lastUpdateTime;
     private Timer timer;
 
-    private GameView ourView;
+    private GameView t;
 
-    /**
-     * Constructs a new GameLoop object and initializes the game components.
-     * Starts the game timer to trigger periodic updates.
-     *
-     * @param view the GameView object for rendering the game state
-     */
-    public GameLoop(final GameView view) {
+    public GameLoop(GameView coso) {
         soundPlayer = new SoundManagerImpl();
         brickWall = new BrickWallImpl(GameInfo.GAME_WIDTH, (int) Math.floor(GameInfo.GAME_HEIGHT * 0.35));
-        balls = new HashSet<>();
+        balls = new HashSet<Ball>();
         balls.add(new Ball());
         brickWall.generateLayout();
-        paddle = new BarImpl(new Point((GameInfo.GAME_WIDTH / 2) - 100, GameInfo.GAME_HEIGHT),
-        new Dimension(200, 5), 0, new Color(0));
+        paddle = new Bar(new Point((GameInfo.GAME_WIDTH / 2)-100, GameInfo.GAME_HEIGHT), new Dimension(200, 5), 0,new Color(0));
         manager = new CollisionManager(balls, brickWall, paddle);
-        ourView = view;
-        ourView.updateGameState(balls, brickWall.getWall(), paddle);
+        t = coso;
+        t.updateGameState(balls, brickWall.getWall(), paddle);
 
         lastUpdateTime = System.nanoTime();
         timer = new Timer(1000 / GameInfo.REFRESH_RATE, this);
         timer.start();
     }
 
-    /**
-     * Invoked when an action occurs. This method is called periodically by the Timer
-     * to update the game state.
-     *
-     * @param e the action event
-     */
     @Override
-    public final void actionPerformed(final ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {
         long currentTime = System.nanoTime();
         long elapsedTime = currentTime - lastUpdateTime;
 
-        if (elapsedTime >= UPDATE_INTERVAL) {
+        if (elapsedTime >= GameInfo.REFRESH_RATE) {
             update();
             lastUpdateTime = currentTime;
         }
     }
 
     /**
-     * Updates the game state, including checking collisions, updating ball positions,
-     * and removing dead entities.
+     * Updates the gamestate
      */
     private void update() {
+
         manager.checkAll();
         this.updateBalls();
         DeathCollector.checkEntities(balls);
         DeathCollector.checkEntities(brickWall.getWall());
     }
 
-    /**
-     * Updates the positions and states of all balls in the game.
-     * Repaints the game view and updates the game state in the view.
-     */
     private void updateBalls() {
         for (var b : balls) {
             b.update();
             // System.out.println(b.toString());
         }
         paddle.move();
-        ourView.repaint();
-        ourView.updateGameState(balls, brickWall.getWall(), paddle);
+        t.repaint();
+        t.updateGameState(balls, brickWall.getWall(), paddle);
     }
 
-    /**
-     * Adds a new ball to the game by duplicating the specified ball.
-     *
-     * @param old the original Ball.
-     */
-    public final void multiplyBall(final Ball old) {
+    public void multiplyBall(Ball old) {
         balls.add(new Ball(old));
     }
+
 }
