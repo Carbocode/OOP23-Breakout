4eb183ac755f3ec62d4c4abbc5e8673fb2963091
Sohail Mama <sohail.mama@studio.unibo.it>
2024-06-24 13:40:54 +0200

    addede comments

--- src/main/java/it/unibo/model/BarExtender.java
+++ src/main/java/it/unibo/model/BarExtender.java
@@ -1,11 +1,29 @@
 package it.unibo.model;
 
-
-
-public class BarExtender {
+/**
+ * This is one of the bonus of the game.
+ * when the bricks that contains it get broken, the bar will extend.
+ */
+public final class BarExtender {
     private static final int INCREASE_AMOUNT = 50;
 
-    public static void extendBar(Bar bar){
+    /**
+     * Barextender constructor.
+     */
+    private BarExtender() {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * this method when it called extends the bar.
+     * 
+     * @param bar
+     */
+    public static void extendBar(final Bar bar) {
         bar.setWidth(bar.getSize().width + INCREASE_AMOUNT);
     }
 
