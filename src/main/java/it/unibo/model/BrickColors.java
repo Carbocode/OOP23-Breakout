1bc30db1b4aff17fefa8a931d8d62fb5af5eac99
Vincenzo Prisco <vincenzo.prisco@studio.unibo.it>
2024-06-26 12:26:41 +0200

    update brick generation

--- src/main/java/it/unibo/model/BrickColors.java
+++ src/main/java/it/unibo/model/BrickColors.java
@@ -3,14 +3,11 @@ package it.unibo.model;
 import java.awt.Color;
 
 public enum BrickColors {
-    RED(new Color(255, 51, 51)),
-    GREEN(new Color(0, 153, 0)),
     BLUE(new Color(32, 46, 133)),
     YELLOW(new Color(51, 51, 255)),
     MAGENTA(new Color(255, 0, 255)),
     LIGHT_BLUE(new Color(51, 255, 255)),
-    PURPLE(new Color(127, 0, 255)),
-    ORANGE(new Color(255, 128, 0));
+    PURPLE(new Color(127, 0, 255));
 
     private final Color color;
 
@@ -18,7 +15,8 @@ public enum BrickColors {
         this.color = color;
     }
 
-    public Color getColor() {
-        return color;
+    public static Color getColor(int row) {
+        BrickColors[] values = BrickColors.values();
+        return values[(row) % values.length].color;
     }
 }
