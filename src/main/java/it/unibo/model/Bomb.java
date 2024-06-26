6dc0a8e80f2221b92e13c3b66a33dabeb4840d6a
Giacomo Ruscelli <giacomo.ruscelli03@gmail.com>
2024-06-26 17:48:47 +0200

    checkstyle fix

--- src/main/java/it/unibo/model/Bomb.java
+++ src/main/java/it/unibo/model/Bomb.java
@@ -5,16 +5,21 @@ import java.awt.Point;
 
 import it.unibo.api.GameEntityImpl;
 
+/**
+ * Bomb power up class.
+ */
 public class Bomb extends GameEntityImpl{
 
-    public Bomb(Point position, Dimension size) {
+    /**
+     * bomb general constructor.
+     * @param position position of the bomb (the same of the ball)
+     * @param size size of the explosion
+     */
+    public Bomb(final Point position, final Dimension size) {
         super(position, size);
     }
-
     @Override
     public void onCollision() {
         // nothing
     }
-    
-    
 }
